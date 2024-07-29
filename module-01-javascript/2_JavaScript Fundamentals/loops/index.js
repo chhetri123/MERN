a4e953654747808d1c4704f7c990966165df0449
Manish Chhetri <chhetridon123@gmail.com>
2024-07-29 06:29:45 +0545

    loops

---
+++ module-01-javascript/2_JavaScript Fundamentals/loops/index.js
@@ -0,0 +1,21 @@
+const names = ["Hari", "Shyam", "Ram", "sita"];
+
+// names.forEach(function (a, index) {
+//   console.log(a);
+// });
+
+// Thought Arrow fucntion
+const num = [1, 2, 4, 5, 9];
+num.forEach((curr, index) => {
+  let c;
+  c = 2 * curr;
+  console.log(c);
+});
+// const string = "Hello";
+// string.forEach(function (currValue, index) {
+//   console.log(currValue);
+// });
+
+// for (let char of names) {
+//   console.log(char);
+// }
