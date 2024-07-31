22d759144024ae312def397a93b68d3eea395fee
nikhilswivt <nikhilpokharel@swivt.com.np>
2024-07-31 14:00:19 +0545

    project assignment

--- module-02-nodejs/Lecture_06/file_compression/server.js
+++ module-02-nodejs/Lecture_06/file_compression/server.js
@@ -1,54 +1,38 @@
 import express from "express";
-import multer from "multer";
-import path from "path";
-import sharp from "sharp";
+import { uploadSingle } from "./middleware/upload.middleware.js";
+import { fileUpload } from "./controller/upload.controller.js";
 import fs from "fs";
-
-const imageStorage = multer.diskStorage({
-  destination: "public/image",
-  filename: (req, file, cb) => {
-    console.log(Date.now() + path.extname(file.originalname));
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({
-  storage: imageStorage,
-  limits: { fileSize: 1000 * 1000 * 10 },
-  fileFilter: (req, file, cb) => {
-    const supportedFormat = [
-      "image/jpg",
-      "image/png",
-      "image/gif",
-      "image/jpeg",
-      "image/svg",
-      "video/mp4",
-    ];
-    if (!supportedFormat.includes(file.mimetype))
-      return cb("File format is not supported", false);
-
-    cb(null, true);
-  },
-});
 
 const app = express();
 
 app.use(express.json());
 
-const uploadSingle = upload.single("doc");
+app.post("/upload", uploadSingle, fileUpload);
+// app.post('/read-me',async(req,res)=>{
+//   console.l
+// })\
 
-app.post("/upload", uploadSingle, async (req, res) => {
-  const compressedImage = await sharp(req.file.path)
-    .resize(200, 200)
-    .toBuffer();
-  sharp(compressedImage).toFile(
-    `compressed-${req.file.originalname}`,
-    (err, info) => {
-      if (!err) fs.unlinkSync(req.file.path);
-      console.log(info);
-    }
-  );
-  res.json({ ok: true });
+app.post("/read-data", (req, res) => {
+  const readStream = fs.createReadStream("me.txt", { highWaterMark: 1 });
+
+  // Set the content type header, adjust it according to your file type
+  res.setHeader("Content-Type", "text/plain");
+
+  readStream.on("data", (dataChunk) => {
+    readStream.resume();
+    res.write(dataChunk);
+  });
+
+  readStream.on("end", () => {
+    res.end();
+  });
+
+  readStream.on("error", (error) => {
+    res.status(500).send("Internal Server Error");
+  });
+
+  // Pipe the read stream to the response
+  // readStream.pipe(res);
 });
 
 app.listen(3030, () => {
