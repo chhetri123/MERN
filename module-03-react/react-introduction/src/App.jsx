71c228f7182f2825bc31897c92c19c8d6b426d37
nikhilswivt <nikhilpokharel@swivt.com.np>
2024-08-06 13:29:44 +0545

    module-03-react: useMemo added

--- module-03-react/react-introduction/src/App.jsx
+++ module-03-react/react-introduction/src/App.jsx
@@ -1,33 +1,11 @@
-import { useState, useMemo } from "react";
 import Layout from "./layout/layout";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const c = useMemo(() => {
-    return calculate(counter);
-  }, [counter]);
-
-  const handleIncrement = () => {
-    setCounter((prev) => prev + 1);
-  };
-  const handleDecrement = () => {
-    setCounter((prev) => prev - 1);
-  };
-
   return (
     <Layout>
-      <p>{c}</p>
-      {/* <button onClick={greet}>Say HI</button> */}
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
+      <h2>Hello</h2>
     </Layout>
   );
-
-  function calculate(num) {
-    // for (let i = 0; i < 1000000000; i++) {}
-    return num * 2;
-  }
 }
 
 export default App;
