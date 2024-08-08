008bddcd4072768895231d7a4b717a22ea5b55a6
nikhilswivt <nikhilpokharel@swivt.com.np>
2024-08-08 10:36:05 +0545

    react: tailwind css introduction

---
+++ module-03-react/react-tailwind/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="container mx-auto leading-10">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel corporis
+        voluptates sint eligendi nisi fugiat exercitationem, quidem unde, ipsa
+        sunt ipsam odit magni qui. Culpa odio quibusdam, vel suscipit quidem
+        repellendus quam. Aut quis voluptatibus ad quod expedita voluptatem
+        reiciendis dolorem possimus. Sint minus est architecto vitae quis labore
+        quas veniam iusto odio, quidem quaerat dignissimos ab? Explicabo iste
+        minus blanditiis accusantium voluptatum dolore ullam numquam debitis
+        illum, aperiam neque consequatur aliquam quibusdam qui quod consequuntur
+        velit minima. Placeat facilis recusandae error neque fugiat, asperiores
+        molestias architecto ipsa. Doloribus blanditiis cum doloremque. Commodi,
+        incidunt consectetur enim quasi nostrum ipsam exercitationem.
+      </p>
+    </div>
+  );
+}
+
+export default App;
